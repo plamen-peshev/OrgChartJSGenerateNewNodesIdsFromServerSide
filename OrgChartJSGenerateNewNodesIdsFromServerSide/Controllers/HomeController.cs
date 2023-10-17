2736059a2f6978ad858ba0628194482b70925fb6
plamen-peshev <plamen.peshev@live.com>
2023-10-17 11:43:25 +0300

    Add project files.

---
+++ OrgChartJSGenerateNewNodesIdsFromServerSide/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace OrgChartJSGenerateNewNodesIdsFromServerSide.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(string id, string pid)
+        {
+            System.Threading.Thread.Sleep(1500);
+
+            Random rnd = new Random();
+            int serverId = rnd.Next();//Generate id from the server it could be SQL server
+
+            return Json(new
+            {
+                id = serverId
+            });
+        }
+    }
+}
